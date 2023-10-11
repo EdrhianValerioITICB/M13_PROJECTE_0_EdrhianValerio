d4d7718cbfc2db0be415cb730505e126d36251d9
Edrhian Valerio <edrhian27@gmail.com>
2023-10-11 16:00:30 +0200

    This Project isn't supposed to work

--- CRUD/src/main/resources/db/schema.sql
+++ CRUD/src/main/resources/db/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS libros (
 	id int PRIMARY KEY AUTO_INCREMENT,
 	titulo varchar(100) NOT NULL,
 	autor varchar(100) NOT NULL,
-	editorioal varchar(50) NOT NULL,
+	editorial varchar(50) NOT NULL,
 	fecha date NOT NULL,
 	tematica varchar(50) NOT NULL
 );
